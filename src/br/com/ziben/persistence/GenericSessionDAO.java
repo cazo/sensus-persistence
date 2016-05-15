baae45adf1e31543316aea3dd5cad81b011cbd6e
Claudio Cardozo <cazo66@gmail.com>
2016-05-14 22:45:01 -0300

    Implementa novos metodos

--- src/br/com/ziben/persistence/GenericSessionDAO.java
+++ src/br/com/ziben/persistence/GenericSessionDAO.java
@@ -16,22 +16,30 @@ along with sensus-persistence.  If not, see <http://www.gnu.org/licenses/>.
 */
 package br.com.ziben.persistence;
 
+import java.io.Serializable;
 import java.lang.reflect.ParameterizedType;
 import java.util.List;
 
+import org.apache.log4j.Logger;
+import org.hibernate.Criteria;
+import org.hibernate.FlushMode;
 import org.hibernate.HibernateException;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.context.internal.ManagedSessionContext;
+import org.hibernate.criterion.Criterion;
 
 /**
- * Class to handle DAOs by a generic hibernate session
+ * Class to handle DAOs by a generic hibernate session factoring
  * @author ccardozo
  *
  * @param <T>
  */
 @SuppressWarnings("unchecked")
 public abstract class GenericSessionDAO<T> {
+	
+	private Logger log = Logger.getLogger("GenericSessionDAO");
 	
     private Session session;
     private Transaction tx;
@@ -44,6 +52,7 @@ public abstract class GenericSessionDAO<T> {
     }
 
     protected void persist(T obj) {
+        log.debug(">>GenericSessionDAO:persist(): " + this.classe.toString());
         try {
             startOperation();
             session.saveOrUpdate(obj);
@@ -52,10 +61,12 @@ public abstract class GenericSessionDAO<T> {
             handleException(e);
         } finally {
             HibernateFactory.close(session);
+            log.debug("<<GenericSessionDAO:persist(): " + this.classe.toString());
         }
     }
 
     protected void delete(T obj) {
+        log.debug(">>GenericSessionDAO:delete(): " + this.classe.toString());
         try {
             startOperation();
             session.delete(obj);
@@ -64,12 +75,13 @@ public abstract class GenericSessionDAO<T> {
             handleException(e);
         } finally {
             HibernateFactory.close(session);
+            log.debug("<<GenericSessionDAO:delete(): " + this.classe.toString());
         }
     }
 
-    protected Object find(String id) {
+    protected Object find(Serializable id) {
+        log.debug(">>GenericSessionDAO:find(): " + this.classe.toString());
         Object obj = null;
-        System.out.println("******* find() >>>>" + this.classe.toString());
         try {
             startOperation();
             obj = session.load(this.classe, id);
@@ -77,13 +89,14 @@ public abstract class GenericSessionDAO<T> {
         } catch (HibernateException e) {
             handleException(e);
         } finally {
-            System.out.println("******* HibernateFactory.close(session) sem close() >>>>" + this.classe.toString());
             HibernateFactory.close(session);
+        	log.debug("<<GenericSessionDAO:find()");
         }
         return obj;
     }
 
     protected List<T> findAll(T clazz) {
+        log.debug(">>GenericSessionDAO:findAll(): " + this.classe.toString());
         List<T> objects = null;
         try {
             startOperation();
@@ -93,19 +106,58 @@ public abstract class GenericSessionDAO<T> {
             handleException(e);
         } finally {
             HibernateFactory.close(session);
+        	log.debug("<<GenericSessionDAO:findAll()");
         }
         return objects;
     }
 
+    protected List<T> findByCriteria(Criterion... criterion) {
+		log.info(">>GenericSessionDAO:findByCriteria()");
+		Criteria crit = null;
+		List<T> list = null;
+		try {
+            startOperation();
+		    crit = session.createCriteria(this.classe);
+		    for (final Criterion c : criterion) {
+		    	crit.add(c);
+		    }
+		    list = crit.list();
+		} catch (HibernateException e) {
+            handleException(e);
+        } finally {
+    		log.info("<<GenericSessionDAO:findByCriteria()");
+            HibernateFactory.close(session);
+        }
+		return list;
+    }
+
+    protected List<?> runQuery(String strQuery) {
+		log.info(">>GenericSessionDAO:runQuery()");
+		List<?> list = null;
+		try {
+            startOperation();
+		    Query query = session.createSQLQuery(strQuery);
+		    list = query.list();
+		} catch (HibernateException e) {
+            handleException(e);
+        } finally {
+    		log.info("<<GenericSessionDAO:runQuery()");
+            HibernateFactory.close(session);
+        }
+		return list;
+    }
+    
     protected void handleException(HibernateException e) throws DataAccessLayerException {
         HibernateFactory.rollback(tx);
         throw new DataAccessLayerException(e);
     }
 
     protected void startOperation() throws HibernateException {
+        log.debug(">>GenericSessionDAO:startOperation()");
         session = HibernateFactory.openSession();
-        //session.setFlushMode(FlushMode.MANUAL);
-        //ManagedSessionContext.bind(session);
+        session.setFlushMode(FlushMode.MANUAL);
+        ManagedSessionContext.bind(session);
         tx = session.beginTransaction();
+        log.debug("<<GenericSessionDAO:startOperation()");
     }
 }
