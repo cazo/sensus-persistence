7b1e30e7682ceec18c9ae03bab99442dda6b822f
ricart.monachesi <rgmonachesi@gmail.com>
2016-11-30 10:27:33 -0200

    Tirei os caracteres especiais de um comentário.

--- src/br/com/ziben/dialectfix/Oracle10gDialectFix.java
+++ src/br/com/ziben/dialectfix/Oracle10gDialectFix.java
@@ -21,7 +21,7 @@ import java.sql.Types;
 import org.hibernate.dialect.Oracle10gDialect;
 
 /**
- * Correção para um bug conhecido mas rejeitado pela equipe do Hibernate<br>
+ * Correcao para um bug conhecido mas rejeitado pela equipe do Hibernate<br>
  * Links de 2 reports que achei no jira do hibernate:<br>
  * <a href=https://hibernate.onjira.com/browse/HHH-5569>https://hibernate.onjira.com/browse/HHH-5569</a><br>
  * <a href=https://hibernate.onjira.com/browse/HHH-6726>https://hibernate.onjira.com/browse/HHH-6726</a>
